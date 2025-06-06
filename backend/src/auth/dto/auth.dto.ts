7a96ee8576adb3f91203a5be731058ed081a9cba
Jürgen Jacobsen <jurgenjacobsen@outlook.com>
2025-06-06 23:35:35 +0100

    🚀 Launching basic backend functions for API

---
+++ backend/src/auth/dto/auth.dto.ts
@@ -0,0 +1,18 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+
+export class AuthDto {
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(4)
+    username: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(8)
+    password: string;
+}
+
